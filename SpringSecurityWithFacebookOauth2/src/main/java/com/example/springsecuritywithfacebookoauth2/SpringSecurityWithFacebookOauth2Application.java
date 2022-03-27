934fd24adb90795feaf923254545d332288bdf00
Ole Ivanskyi <olehfromukraine@gmail.com>
2022-03-27 20:02:37 +0300

    Created Spring Security project with Facebook Oauth2

---
+++ SpringSecurityWithFacebookOauth2/src/main/java/com/example/springsecuritywithfacebookoauth2/SpringSecurityWithFacebookOauth2Application.java
@@ -0,0 +1,13 @@
+package com.example.springsecuritywithfacebookoauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityWithFacebookOauth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityWithFacebookOauth2Application.class, args);
+    }
+
+}
