7815e156fa985bebf92d907129fbe571ecdc928d
Ole Ivanskyi <olehfromukraine@gmail.com>
2022-03-27 16:11:15 +0300

    created project to work with MySQL

---
+++ SpringSecurityAndMySQL/src/main/java/com/example/springsecurityandmysql/SpringSecurityAndMySqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityandmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAndMySqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityAndMySqlApplication.class, args);
+    }
+
+}
---
+++ SpringSecurityAndMySQL/src/test/java/com/example/springsecurityandmysql/SpringSecurityAndMySqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityandmysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAndMySqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
